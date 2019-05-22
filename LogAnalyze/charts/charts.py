73479b277dc2da650662e264e22ee0225820b236
WangJunXiong <1411349759@qq.com>
2019-05-22 22:36:36 +0800

    Add files via upload

---
+++ LogAnalyze/charts/charts.py
@@ -0,0 +1,24 @@
+# encoding:utf-8
+"""
+生成图表
+============
+Date@2019/05/21
+Author@wangjunxiong
+"""
+
+
+class generate_charts(object):
+    """generate charts and final file"""
+    def __init__(self, src_file):
+        """
+        :param src_file:
+        :return charts and final file
+        """
+
+    def generate(self):
+        """ format
+        """
+
+    def fuck_end(self):
+        pass
+
