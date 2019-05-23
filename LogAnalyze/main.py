3df5329adab1f4f06856806e746c5d2d5537aa5f
WangJunXiong <1411349759@qq.com>
2019-05-23 21:58:08 +0800

    Update main.py

--- LogAnalyze/main.py
+++ LogAnalyze/main.py
@@ -19,6 +19,7 @@ class log_analyze(object):
         :param method:
         :return: 根据method生成第一阶段目标文件
         """
+        print "文件预处理..."
         with open(self.filename, 'r') as self.f:
             if method is 1:
                 rst = open("rst_1.txt", 'w')
@@ -29,7 +30,7 @@ class log_analyze(object):
                         flag = tmp[-1].index("：")
                         d = tmp[0][5:7] + tmp[0][-2:]
                         r = d + " " + tmp[1][:8] + " " + tmp[5][:-1] + " " + tmp[-1][flag+21:]
-                        print(flag)
+                        # print(flag)
                     except:
                         d = tmp[0][5:7] + tmp[0][-2:]
                         r = d + " " + tmp[1][:8] + " " + tmp[5][:-1] + " " + tmp[-1][-7:]
@@ -44,7 +45,7 @@ class log_analyze(object):
                         try:
                             flag = tmp[-1].index("：")
                             r = tmp[0] + " " + tmp[1][:8] + " " + tmp[5][:-1] + " " + tmp[-1][flag+9:]
-                            print(flag)
+                            # print(flag)
                         except:
                             r = tmp[0] + " " + tmp[1][:8] + " " + tmp[5][:-1] + " " + tmp[-1][-7:]
                     elif len(tmp) == 8:
@@ -59,13 +60,16 @@ class log_analyze(object):
         """
         :return: 根据第一阶段的目标文件生成统计文件
         """
+        print "生成第一阶段目标文件..."
         generate_statistical().generate_start("rst_1.txt", polling=False)
-        self.generate_charts()
+        self.generate_chart()
 
-    def generate_charts(self):
+    def generate_chart(self):
         """
         :return: 根据生成的统计文件产生图表
         """
+        print "生成图表..."
+        generate_charts().generate_data()
         pass
 
     def final_action(self):
@@ -77,6 +81,6 @@ if __name__ == '__main__':
     # parser方法中使用 1 进行预处理
     # 日志模式包含 2019-05-20 14:26:16,569 - obtaining_domains_ip_cname_by_ns.py[line:82] - WARNING: 无法获取域名:www.sastt.pw 记录
     # parser方法中使用 2 进行预处理
-    filename = "obtaining_domain_ns.log"
-    # filename = "obtaining_domains_ip_cname_by_ns.log"  # 要处理的日志文件路径
+    #filename = "obtaining_domain_ns.log"
+    filename = "obtaining_domains_ip_cname_by_ns.log"  # 要处理的日志文件路径
     log_analyze(filename).generate_target_file(method=1)
